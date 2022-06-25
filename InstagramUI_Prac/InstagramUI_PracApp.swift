9ddf4ff68e22ca6f16bd5a92643142498cc2ccbc
Yen Hung <yen4meter@gmail.com>
2022-06-25 19:17:35 +0800

    Initial Commit

---
+++ InstagramUI_Prac/InstagramUI_PracApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramUI_PracApp.swift
+//  InstagramUI_Prac
+//
+//  Created by user on 2022/6/25.
+//
+
+import SwiftUI
+
+@main
+struct InstagramUI_PracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
